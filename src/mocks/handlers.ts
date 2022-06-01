c9c18d0803e82704c05ca33795e4048bd2c8dcb2
jindonyy <rsefa94@gmail.com>
2022-06-01 22:35:13 +0900

    Feat: 체크인, 체크아웃에 따라 데이터 get API 추가
    jindonyy/airbnb/issue#2

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,9 +1,31 @@
 import { rest } from 'msw';
+import { MILLISECOND_FOR_ONE_DAY } from '@constants/date';
 
 import accommodations from './accommodations';
 
+const getDataToPeriod = (checkin: string, checkout: string) => {
+  const [, , checkinDay] = checkin.split('-').map(Number);
+  const checkinToTime = new Date(checkin).getTime();
+  const checkoutToTime = new Date(checkout).getTime();
+  const period = (checkoutToTime - checkinToTime) / MILLISECOND_FOR_ONE_DAY;
+  const dataToPeriod = [];
+
+  for (let count = checkinDay - 1; count < period; count += 1) {
+    const idx = count % accommodations.length;
+    dataToPeriod.push(accommodations[idx]);
+  }
+
+  return dataToPeriod;
+};
+
 export const handlers = [
-  rest.get('/testdata', (req, res, ctx) => {
+  rest.get('/reservation', (req, res, ctx) => {
+    const checkin = req.url.searchParams.get('checkin');
+    const checkout = req.url.searchParams.get('checkout');
+    if (checkin && checkout) {
+      const dataToPeriod = getDataToPeriod(checkin, checkout);
+      return res(ctx.status(200), ctx.json(dataToPeriod));
+    }
     return res(ctx.status(200), ctx.json(accommodations));
   })
 ];
