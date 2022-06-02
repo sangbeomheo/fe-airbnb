f37426bedce6413a700b1529fff39ec6138cacbc
Sangbeom Heo <mansaout@gmail.com>
2022-06-02 17:58:51 +0900

    feat: arrow 아이콘 버튼 생성
    jindonyy/airbnb/issue#6

--- src/components/common/IconButton.tsx
+++ src/components/common/IconButton.tsx
@@ -2,23 +2,29 @@ import React, { FunctionComponent } from 'react';
 import { ReactComponent as Menu } from '@assets/images/icon_menu.svg';
 import { ReactComponent as Search } from '@assets/images/icon_search.svg';
 import { ReactComponent as Xcircle } from '@assets/images/icon_xCircle.svg';
+import { ReactComponent as ChevronLeft } from '@assets/images/icon_chevronLeft.svg';
+import { ReactComponent as ChevronRight } from '@assets/images/icon_chevronRight.svg';
+
 import { Button } from '@components/common/IconButton.style';
 
 interface Props {
   icon: string;
+  handleClick?: FunctionComponent;
 }
 
 const icons: { [key: string]: FunctionComponent } = {
   menu: Menu,
   search: Search,
-  xCircle: Xcircle
+  xCircle: Xcircle,
+  chevronLeft: ChevronLeft,
+  chevronRight: ChevronRight
 };
 
-export default function IconButton({ icon }: Props) {
+export default function IconButton({ icon, handleClick }: Props) {
   const Icon = icons[icon];
 
   return (
-    <Button>
+    <Button onClick={handleClick}>
       <Icon />
     </Button>
   );
