d3c13c216e3e658a4ebec29a9c8efb2e60e37df3
Sangbeom Heo <mansaout@gmail.com>
2022-06-07 12:20:18 +0900

    feat: 서치바의 초기화버튼(x) 클릭 시 해당 예약내역이 초기화(체크인, 체크아웃 구현)
    jindonyy/airbnb/issue#15

--- src/components/SearchBar/SearchInputButton.tsx
+++ src/components/SearchBar/SearchInputButton.tsx
@@ -7,28 +7,55 @@ import {
   PlaceHolder
 } from '@components/SearchBar/SearchInputButton.style';
 import { SelectedModalNameContext } from '@contexts/SelectedModalNameProvider';
+import { ReservationInfoContext } from '@contexts/ReservationInfoProvider';
+import { getStringDate } from '@/utils/util';
 
 interface Props {
   name: string;
   label: string;
-  value?: string;
+  value?: string | null;
   placeholder: string;
   searchName: string;
-  hasCloseBtn?: boolean;
-  hasBorderLeft?: boolean;
 }
 
-function SearchInputButton({
-  name,
-  label,
-  value = '',
-  placeholder,
-  searchName,
-  hasCloseBtn = true,
-  hasBorderLeft = true
-}: Props) {
+function SearchInputButton({ name, label, value = '', placeholder, searchName }: Props) {
   const { selectedModalName, showSearchModal } = useContext(SelectedModalNameContext);
+  const { reservationInfo, setReservationInfo } = useContext(ReservationInfoContext);
   const [focus, setFocus] = useState(false);
+
+  const resetValue = (searchName: string) => {
+    switch (searchName) {
+      case 'checkin':
+        setReservationInfo({
+          ...reservationInfo,
+          period: {
+            checkin: null,
+            checkout: reservationInfo.period.checkout
+          }
+        });
+        break;
+
+      case 'checkout':
+        setReservationInfo({
+          ...reservationInfo,
+          period: {
+            checkin: reservationInfo.period.checkin,
+            checkout: null
+          }
+        });
+        break;
+
+      case 'price':
+        console.log('price');
+        break;
+
+      case 'personal':
+        console.log('personal');
+        break;
+
+      default:
+    }
+  };
 
   useEffect(() => {
     if (selectedModalName === searchName) setFocus(true);
@@ -36,12 +63,12 @@ function SearchInputButton({
   }, [selectedModalName]);
 
   return (
-    <Container hasBorderLeft={hasBorderLeft}>
+    <Container focus={focus}>
       <InputButton type="button" name={name} onClick={() => showSearchModal(searchName)}>
         <Label focus={focus}>{label}</Label>
         <div>{value || <PlaceHolder>{placeholder}</PlaceHolder>}</div>
       </InputButton>
-      {hasCloseBtn && value && <IconButton icon="xCircle" />}
+      {value && <IconButton icon="xCircle" handleClick={() => resetValue(searchName)} />}
     </Container>
   );
 }
