87f170821cd6c25ec744628e0eff513c877c4088
Sangbeom Heo <mansaout@gmail.com>
2022-06-07 12:19:45 +0900

    feat: 예약내역이 변경될 때마다 서치바의 Input Value에 업데이트됌
    jindonyy/airbnb/issue#15

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -7,26 +7,53 @@ import PriceModal from '@components/Modal/PriceModal';
 import IconButton from '@components/common/IconButton';
 import SearchInputButton from '@components/SearchBar/SearchInputButton';
 import { PERSONS_TEXTS } from '@/constants';
+import { addCommasToNumber } from '@/utils/util';
 
 const searchInputButtonsInfo = [
   {
     name: '체크인',
     label: '체크인',
     placeholder: '날짜 입력',
-    searchName: 'checkin',
-    hasCloseBtn: false,
-    hasBorderLeft: false
+    searchName: 'checkin'
   },
   {
     name: '체크아웃',
     label: '체크아웃',
     placeholder: '날짜 입력',
-    searchName: 'checkout',
-    hasBorderLeft: false
+    searchName: 'checkout'
   },
-  { name: '요금', label: '요금', placeholder: '금액대 설정', searchName: 'price' },
-  { name: '인원', label: '인원', placeholder: '게스트 추가', searchName: 'persons' }
+  {
+    name: '요금',
+    label: '요금',
+    placeholder: '금액대 설정',
+    searchName: 'price'
+  },
+  {
+    name: '인원',
+    label: '인원',
+    placeholder: '게스트 추가',
+    searchName: 'persons'
+  }
 ];
+
+const getReservationPriceText = price => {
+  const { min, max } = price;
+  if (!min && !max) return '';
+  return `${addCommasToNumber(min)}~${addCommasToNumber(max)}원`;
+};
+
+const getReservationPersonnelText = persons => {
+  const { adult, child, infant } = persons;
+  if (!adult && !child && !infant) return '';
+
+  const reservationPersonnelTexts = Object.keys(persons).reduce((text, ageGroup, idx) => {
+    const numberForAgeGroup = persons[ageGroup as keyof typeof persons];
+
+    return text + (numberForAgeGroup > 0 ? `${PERSONS_TEXTS[idx]} ${numberForAgeGroup}명, ` : '');
+  }, '');
+
+  return reservationPersonnelTexts;
+};
 
 function SearchBar() {
   const { reservationInfo } = useContext(ReservationInfoContext);
@@ -51,40 +78,28 @@ function SearchBar() {
 
   useEffect(getSelectedModal, [selectedModalName]);
 
-  const getReservationPersonnelText = () => {
-    const reservationPersonnelTexts = Object.keys(reservationInfo.persons).reduce(
-      (text, ageGroup, idx) => {
-        const numberForAgeGroup =
-          reservationInfo.persons[ageGroup as keyof typeof reservationInfo.persons];
-
-        return (
-          text + (numberForAgeGroup > 0 ? `${PERSONS_TEXTS[idx]} ${numberForAgeGroup}명, ` : '')
-        );
-      },
-      ''
-    );
-
-    return reservationPersonnelTexts;
-  };
-
   const hasReservationDate = reservationInfo.period.checkin && reservationInfo.period.checkout;
+
+  const searchInputButtonsValue = [
+    reservationInfo.period.checkin,
+    reservationInfo.period.checkout,
+    getReservationPriceText(reservationInfo.price),
+    getReservationPersonnelText(reservationInfo.persons)
+  ];
 
   return (
     <>
       <Container className="searchBar">
-        {searchInputButtonsInfo.map(
-          ({ name, label, placeholder, searchName, hasCloseBtn, hasBorderLeft }, idx) => (
-            <SearchInputButton
-              key={idx}
-              name={name}
-              label={label}
-              placeholder={placeholder}
-              searchName={searchName}
-              hasCloseBtn={hasCloseBtn}
-              hasBorderLeft={hasBorderLeft}
-            />
-          )
-        )}
+        {searchInputButtonsInfo.map(({ name, label, placeholder, searchName }, idx) => (
+          <SearchInputButton
+            value={searchInputButtonsValue[idx]}
+            key={idx}
+            name={name}
+            label={label}
+            placeholder={placeholder}
+            searchName={searchName}
+          />
+        ))}
         <SearchButtonWrap disabled={!hasReservationDate}>
           <IconButton icon="search" disabled={!hasReservationDate}>
             {hasReservationDate ? <SearchText>검색</SearchText> : undefined}
